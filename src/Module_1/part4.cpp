6b88ea5de7aa2df9be9dcaf34df47f67afa8e5b6
Nayony <alexfadeyev2007@gmail.com>
2026-04-26 11:51:00 +0300

    fixed

--- src/Module_1/part4.cpp
+++ src/Module_1/part4.cpp
@@ -1,14 +1,14 @@
 #include "part0.h"
 #include "part2.h"
 #include "part3.h"
+
 int NZER_N_B(const big_N& n) {
-    for (int i = 0; i < n.digits.size(); i++) {
-        if (n.digits[i] != 0) {
-            return 1;
-        }
+    if (n.digits.size() == 1 && n[0] == 0){
+        return 0;
     }
-    return 0; 
+    return 1;
 }
+
 big_N ADD_1N_N(big_N n){
     int i =0;
     while(i < n.digits.size()){
@@ -24,6 +24,7 @@ big_N ADD_1N_N(big_N n){
     n.digits.push_back(1);
     return n;
 }
+
 big_N ADD_NN_N(big_N n1, big_N n2) {
     big_N result("");
     result.digits.clear(); 
@@ -44,11 +45,12 @@ big_N ADD_NN_N(big_N n1, big_N n2) {
     
     return result;
 }
-big_N SUB_NDN_N(big_N n1, big_N n2,int k){
-    n2 = MUL_ND_N(n2,k);
-    int cmp = COM_NN_D(n1,n2);
-    if (cmp == 2 || cmp == 0){
-        return SUB_NN_N(n1,n2);
+
+big_N SUB_NDN_N(const big_N& n1, const big_N& n2, int k) {
+    big_N mul_n2 = MUL_ND_N(n2, k);
+    int cmp = COM_NN_D(n1, mul_n2);
+    if (cmp == 2 || cmp == 0) {
+        return SUB_NN_N(n1, mul_n2);
     }
     return big_N("");
 }
