29b57bd3ce25bfbb87a01c4edca0a69d211827f6
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2026-04-27 21:38:40 +0300

    make code not scary

--- src/Module_2/part1.cpp
+++ src/Module_2/part1.cpp
@@ -5,7 +5,7 @@
 #include "part3.h"
 #include "../Module_1/part4.h"
 
-char POZ_Z_D(const big_Z& z) {
+char SGN_Z_D(const big_Z& z) {
     if (z.digits.size() == 1 && z.digits[0] == 0)
         return 0;
     if (z.sign == 1)
@@ -14,26 +14,23 @@ char POZ_Z_D(const big_Z& z) {
 }
 
 big_Z DIV_ZZ_Z(const big_Z& x, const big_Z& y) {
-    if (POZ_Z_D(y) == 0)
-        return big_Z {};
-    if (POZ_Z_D(x) == 0)
-        return big_Z {"0"};
+    if (SGN_Z_D(y) == 0)
+        return big_Z();
+    if (SGN_Z_D(x) == 0)
+        return big_Z("0");
 
-    big_N abs_x {ABS_Z_N(x)};
-    big_N abs_y {ABS_Z_N(y)};
+    big_N abs_x = ABS_Z_N(x);
+    big_N abs_y = ABS_Z_N(y);
 
-    big_N quotinent {DIV_NN_N(abs_x, abs_y)};
+    big_N quotinent = DIV_NN_N(abs_x, abs_y);
 
-    if (POZ_Z_D(x) == POZ_Z_D(y)) {
-        big_Z res;
+    big_Z res;
+    if (SGN_Z_D(x) == SGN_Z_D(y))
         res.sign = 1;
-        res.digits = quotinent.digits;
-        return res;
-    } else {
+    else {
         quotinent = ADD_1N_N(quotinent);
-        big_Z res;
         res.sign = 0;
-        res.digits = quotinent.digits;
-        return res;
     }
+    res.digits = quotinent.digits;
+    return res;
 }
--- src/Module_2/part1.h
+++ src/Module_2/part1.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "part0.h"
 
-char POZ_Z_D(const big_Z& z);
+char SGN_Z_D(const big_Z& z);
 big_Z DIV_ZZ_Z(const big_Z& x, const big_Z& y);
