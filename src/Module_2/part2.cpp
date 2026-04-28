3def615334342224d2c40c4001ecc9882ad8dc44
AndruyGO <a_zeybel@bk.ru>
2026-04-28 19:21:37 +0300

    bug fix3

--- src/Module_2/part2.cpp
+++ src/Module_2/part2.cpp
@@ -1,5 +1,6 @@
-#include "../Module_1/natural.h"
 #include "integer.h"
+#include "../Module_1/natural.h"
+
 
 big_Z TRANS_N_Z(const big_N &n) {
     if (!NZER_N_B(n)) 
@@ -11,19 +12,21 @@ big_N TRANS_Z_N(const big_Z &z) {
     if (SGN_Z_D(z) == -1) return big_N();
     return big_N(z.digits);
 }
-
 big_Z MOD_ZZ_Z(const big_Z &a, const big_Z &b) {
     if (SGN_Z_D(b) == 0) return big_Z();
     if (SGN_Z_D(a) == 0) return big_Z("0");
 
     big_Z k = DIV_ZZ_Z(a, b);
-
-    // Делимое больше 0
-    if (SGN_Z_D(a) == 1) 
-        return SUB_ZZ_Z(a, MUL_ZZ_Z(b, k));
-
-    // Делимое меньше 0
-    return ADD_ZZ_Z(ABS_Z_Z(MUL_ZZ_Z(b, k)), a);
-
-
+    
+    // 1. УБРАЛ условный оператор if/else
+    // 2. ВСЕГДА вычисляем r = a - b*k (это работает для любых знаков)
+    big_Z r = SUB_ZZ_Z(a, MUL_ZZ_Z(b, k));
+    
+    // 3. ДОБАВИЛ проверку и коррекцию отрицательного остатка
+    if (SGN_Z_D(r) == -1) {
+        big_Z abs_b = ABS_Z_Z(b);
+        r = ADD_ZZ_Z(r, abs_b);
+    }
+    
+    return r;
 }
--- src/Module_2/part2.h
+++ src/Module_2/part2.h
@@ -1,9 +1,7 @@
 #pragma once
 #include "part0.h"
-
+#include "../Module_1/part0.h"
 
 big_Z TRANS_N_Z(const big_N &n);
-
 big_N TRANS_Z_N(const big_Z &z);
-
 big_Z MOD_ZZ_Z(const big_Z &a, const big_Z &b);
