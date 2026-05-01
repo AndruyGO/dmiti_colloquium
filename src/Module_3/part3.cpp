996d2ddbb894ec67b2b7f8673f2053758e633849
BW.2gp <104030465+Banan41k2gp@users.noreply.github.com>
2026-05-01 15:00:28 +0300

    removed red in part3.cpp

--- src/Module_3/part3.cpp
+++ src/Module_3/part3.cpp
@@ -1,18 +1,17 @@
 #include "part3.h"
 #include "part0.h"
-#include "part2.h"
 #include "../Module_1/natural.h"
 
 big_Q MUL_QQ_Q(const big_Q& x, const big_Q& y) {
     big_Q result = big_Q(x.sign * y.sign, MUL_NN_N(x.up, y.up), MUL_NN_N(x.down, y.down));
     if (NZER_N_B(result.up) == 0)
         result.sign = 1;
-    return RED_Q_Q(result);
+    return result;
 }
 
 big_Q DIV_QQ_Q(const big_Q& x, const big_Q& y) {
     big_Q result = big_Q(x.sign * y.sign, MUL_NN_N(x.up, y.down), MUL_NN_N(x.down, y.up));
     if (NZER_N_B(result.up) == 0)
         result.sign = 1;
-    return RED_Q_Q(result);
+    return result;
 }
