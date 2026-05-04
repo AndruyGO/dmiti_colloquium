ce29b224e0785126d7f423fe2f9dec311fd2a797
AndruyGO <a_zeybel@bk.ru>
2026-05-04 08:04:41 +0300

    bug fix1

--- src/Module_4/part3.cpp
+++ src/Module_4/part3.cpp
@@ -1,7 +1,8 @@
-#include "part 3.h"
+#include "part3.h"
 #include "part0.h"
 #include "../Module_1/natural.h"
 #include "../Module_2/integer.h"
+#include "../Module_3/rational.h"
 
 big_N DEG_P_N(const big_P& p) {
     if (p.monomials.empty()) {
@@ -10,20 +11,23 @@ big_N DEG_P_N(const big_P& p) {
     return p.monomials[0].degree;
 }
 
-big_Q FAC_P_Q(big_P& p) {
+big_Q FAC_P_Q(const big_P &p) {
     if (p.monomials.empty()) {
         return big_Q("0");
     }
-    big_N up = ABS_Z_N(p.monomials[0].val.up);
+    big_N up = p.monomials[0].val.up;
     big_N down = p.monomials[0].val.down;
     for (size_t i = 1; i < p.monomials.size(); i++) {
-        up = GCF_NN_N(up, ABS_Z_N(p.monomials[i].val.up));
+        up = GCF_NN_N(up, p.monomials[i].val.up);
         down = LCM_NN_N(down, p.monomials[i].val.down);
     }
-    big_Q div = big_Q(TRANS_N_Z(up), TRANS_N_Z(down));
+    big_Q div = big_Q((char)1 , up, down);
+    return div;
+}
+
+big_P DIVQ_PQ_P (big_P p, const big_Q &div){
     for (size_t i = 0; i < p.monomials.size(); i++) {
         p.monomials[i].val = DIV_QQ_Q(p.monomials[i].val, div);
     }
-
-    return div;
+    return p;
 }
--- src/Module_4/part3.h
+++ src/Module_4/part3.h
@@ -1,4 +1,5 @@
 #include "part0.h"
 
 big_N DEG_P_N(const big_P& p);
-big_Q FAC_P_Q(big_P& p);
+big_Q FAC_P_Q(const big_P& p);
+big_P DIVQ_PQ_P(big_P p, const big_Q &div);
