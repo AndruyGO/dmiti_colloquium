0898e81648f26c6259501e0cd1e05864c9eda0cf
AndruyGO <a_zeybel@bk.ru>
2026-05-09 13:56:39 +0300

    bug fix1

--- src/Module_4/part3.2.h
+++
@@ -1,4 +0,0 @@
-#include "part0.h"
-
-big_P MUL_Pxk_P(const big_P& p, const big_N& k);
-big_P MUL_PP_P(const big_P& p1, const big_P& p2)
--- src/Module_4/part3.h
+++ src/Module_4/part3.h
@@ -1,5 +1,6 @@
 #include "part0.h"
 
+big_P MUL_Pxk_P(const big_P& p, const big_N& k);
+big_P MUL_PP_P(const big_P& p1, const big_P& p2);
 big_N DEG_P_N(const big_P& p);
-big_Q FAC_P_Q(const big_P& p);
-big_P DIV_PQ_P(big_P p, const big_Q &div);
+big_Q FAC_P_Q(const big_P &p);
